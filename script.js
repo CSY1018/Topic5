f303e87cdb59d65ef664c0c81a4aa3ba7f2e2420
Tom Butler <tom@r.je>
2017-02-12 15:16:09 +0000

    uses an array to store english words

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
 function diceClick() {
-       var diceRoll = Math.ceil(Math.random() * 6);
-       this.className = 'side' + diceRoll;
-	   this.nextSibling.nextSibling.firstChild.nodeValue = 'You rolled a ' + diceRoll;
+        var numbers = [];
+        numbers[1] = 'One';
+        numbers[2] = 'Two';
+        numbers[3] = 'Three';
+        numbers[4] = 'Four';
+        numbers[5] = 'Five';
+        numbers[6] = 'Six';
+
+        var diceRoll = Math.ceil(Math.random() * 6);
+        this.className = 'side' + diceRoll;
+
+        this.nextSibling.nextSibling.firstChild.nodeValue = 'You rolled a ' + numbers[diceRoll];
 }
 
 function myLoadEvent() {
