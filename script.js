171a4e75ca6ce6736fd81bb47d586cf97c093dee
Tom Butler <tom@r.je>
2017-02-12 15:17:13 +0000

    animates the dice

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
-function diceClick() {
+var interval = 0;
+var stopInterval = 0;
+var currentDice = 0;
+
+function animateDice() {
+        var diceRoll = Math.ceil(Math.random() * 6);
+        currentDice.className = 'side' + diceRoll;
+}
+function stopAnimation() {
+        clearInterval(interval);
+        clearInterval(stopInterval);
+
         var numbers = [];
         numbers[1] = 'One';
         numbers[2] = 'Two';
@@ -8,16 +19,18 @@ function diceClick() {
         numbers[6] = 'Six';
 
         var diceRoll = Math.ceil(Math.random() * 6);
-        this.className = 'side' + diceRoll;
-
-        this.nextSibling.nextSibling.firstChild.nodeValue = 'You rolled a ' + numbers[diceRoll];
+        currentDice.className = 'side' + diceRoll;
+        currentDice.nextSibling.nextSibling.firstChild.nodeValue = 'You rolled a ' + numbers[diceRoll];
 }
-
+function diceClick() {
+        currentDice = this;
+        interval = setInterval(animateDice, 100);
+        stopInterval = setInterval(stopAnimation, 2000);
+}
 function myLoadEvent() {
         var elements = document.getElementsByTagName('div');
         for (var i = 0; i < elements.length; i++) {
                 elements[i].addEventListener('click', diceClick);
         }
 }
-
 document.addEventListener('DOMContentLoaded', myLoadEvent);
